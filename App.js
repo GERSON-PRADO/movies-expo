5d97be74b3ce7143d38a318c02183816b2aacc4b
GERSON-PRADO <devpgto@gmail.com>
2022-06-19 17:05:44 +0100

    Escondendo a StatusBar

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
+import {StatusBar} from 'react-native';
 
 function App() {
   return (
     <NavigationContainer>
+      <StatusBar higgen={true}/>
       <Routes />
     </NavigationContainer>
   )
